7bfebf05f1818d7edfaf4fce6f4c2c732514ad73
DCS-Robinson <san.vitale@gmail.com>
2015-08-23 10:00:20 -0700

    added code to cachematrix.R and added .Rhistory

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions cooperate to cache the inverse of a square invertible matrix
 
-## Write a short comment describing this function
-
+## Takes the matrix to be inverted and returns an object that stores the matrix
+## as well as it's inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        inverse_mat <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                
+                inverse_mat <<- NULL
+        }
+        
+        get <- function() x
+        
+        set_inverse_mat <- function(calculated_inverse) inverse_mat <<- calculated_inverse
+        
+        get_inverse_mat <- function() inverse_mat
+        
+        list(set = set, get = get, set_inverse_mat = set_inverse_mat, get_inverse_mat = get_inverse_mat)
+        
 }
 
 
-## Write a short comment describing this function
+## Takes the matrix cache returns the inverse matrix. It calculates and stores
+## the invere only if it does not already exist in the cache. Otherwise, it simply
+## retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse_mat <- x$get_inverse_mat()
+        
+        if(!is.null(inverse_mat)){
+                print("retrieving inverse matrix")
+                return(inverse_mat)
+        }
+        argument_matrix <- x$get()
+        inverse_mat <- solve(argument_matrix)
+        
+        x$set_inverse_mat(inverse_mat)
+        
+        inverse_mat
 }
